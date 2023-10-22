cfa0a527268b071dc20f1f51911b72daec9fa50a
shiinazzz <hkhanhnam81@gmail.com>
2023-10-22 17:31:04 +0700

    Disable EPPE
    Change-Id: I6d70d468292753e093476e59bf12238d65b66897

--- lineage_flashlm.mk
+++ lineage_flashlm.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Disable EPPE enforcement
+TARGET_DISABLE_EPPE := true
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
